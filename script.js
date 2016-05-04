9c27718068ebd248f300abd145d10958564def47
heladolk <heladolk@yahoo.com>
2016-05-04 16:02:36 -0700

    created generateRandomColor function

--- script.js
+++ script.js
@@ -9,18 +9,22 @@ var body = document.getElementsByTagName("body")[0];
 // column * row Checkerboard
 for (var i = 0; i < row; i++) {
 	for (var j = 0; j < col; j++) {
-		var rgbColor, tileColor, tile;
-		
+		var tile;
+	
 		tile = document.createElement('div');
 		tile.style.width = "11.1%";
 		tile.style.float = "left";
-		tile.style.paddingBottom = "11.1%";
+		tile.style.paddingBottom = "11.1%";		
+		tile.style.backgroundColor = generateRandomColor();
 		
-		// generate random RGB colors in format rgb(num1, num2, num3)
-		rgbColor = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
-		tileColor = "rgb(" + rgbColor.join(",") + ")"
-		tile.style.backgroundColor = tileColor;
 		body.appendChild(tile);
 	}
 }
 
+// generate random RGB colors in format rgb(num1, num2, num3)
+function generateRandomColor() {
+	var rgbColor = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
+	var tileColor = "rgb(" + rgbColor.join(",") + ")"
+
+	return tileColor;
+}
