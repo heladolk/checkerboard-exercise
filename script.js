2a74401077b37a8090a4df8513120125449adba8
heladolk <heladolk@yahoo.com>
2016-05-04 15:57:29 -0700

    initial commit

--- script.js
+++ script.js
@@ -4,40 +4,39 @@
 var row = 7;
 var col = 9;
 
-var rgbColor1 = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
-var rgbColor2 = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
-
 var body = document.getElementsByTagName("body")[0];
 
 // column * row Checkerboard
-for (var i = 0; i < row; i++) {
-	for (var j = 0; j < col; j++) {
-		var tile, tileColor1, tileColor2;
-		var gradient1, gradient2;
-		
-		tile = document.createElement('div');
-		tile.style.width = "11.1%";
-		tile.style.float = "left";
-		tile.style.paddingBottom = "11.1%";
-
-		if ( (i + j) % 2 === 0) {
+function drawCheckerBoard() {
+	for (var i = 0; i < row; i++) {
+		for (var j = 0; j < col; j++) {
+			var rgbColor, tileColor, tile;
+			
+			tile = document.createElement('div');
+			tile.style.width = "11.1%";
+			tile.style.float = "left";
+			tile.style.paddingBottom = "11.1%";
+			
 			// generate random RGB colors in format rgb(num1, num2, num3)
-			gradient1 = Math.floor(Math.random() * 256);
-			// change green component
-			rgbColor1[1] = Math.floor((rgbColor1[1] + gradient1)/2); 
-			tileColor1 = "rgb(" + rgbColor1.join(",") + ")";
-			tile.style.backgroundColor = tileColor1;			
-		} else {
-			gradient2 = Math.floor(Math.random() * 256);
-			// change black component
-			rgbColor2[2] = Math.floor((rgbColor2[2] + gradient2)/2); 
-			tileColor2 = "rgb(" + rgbColor2.join(",") + ")";
-			tile.style.backgroundColor = tileColor2;
+			tile.style.backgroundColor = pickColor();
+
+			body.appendChild(tile);
 		}
-		
-		body.appendChild(tile);
-	}
+	};
 }
+
+function pickColor() {
+	var rgbColor = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
+	var tileColor = "rgb(" + rgbColor.join(",") + ")"
+	return tileColor;
+}
+
+drawCheckerBoard();
+
+// change color every 2 seconds
+setTimeout(drawCheckerBoard(), 2000);
+setTimeout(drawCheckerBoard(), 4000);
+setTimeout(drawCheckerBoard(), 6000);
 
 
 
